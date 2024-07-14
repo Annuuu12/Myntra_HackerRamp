160db110a9a63b308ac370adef33af4abc5f0768
arpitak11 <157416586+arpitak11@users.noreply.github.com>
2024-07-15 00:10:34 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/explore" element={<Explore />} />
+          <Route path="/inbox" element={<Inbox />} />
           <Route path="/trending" element={<Trending />} />
           <Route path="/profiles" element={<Profiles />} />
         </Routes>
